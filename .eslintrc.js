29d721d06d628a9d6f6133a4bd87318bbc2fa1b6
2ricky <wqscut@gmail.com>
2019-11-27 23:43:32 +0800

    build: setup prettier with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,38 @@
 module.exports = {
-    "root": true,
-    "env": {
-        "browser": true,
-        "es6": true
+  root: true,
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:jest/recommended',
+    'standard',
+    'plugin:prettier/recommended'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "extends": [
-        "plugin:react/recommended",
-        "plugin:jest/recommended",
-        "standard"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "jest",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    },
-    // https://github.com/typescript-eslint/typescript-eslint/issues/46#issuecomment-470486034
-    overrides: [
-        {
-          files: ['*.ts', '*.tsx'],
-          rules: {
-            '@typescript-eslint/no-unused-vars': [2, { args: 'none' }]
-          }
-        }
-      ]
-};
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'jest', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error'
+  },
+  // https://github.com/typescript-eslint/typescript-eslint/issues/46#issuecomment-470486034
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': [2, { args: 'none' }]
+      }
+    }
+  ]
+}
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  semi: false,
+  bracketSpacing: true,
+  parser: 'babylon'
+}
