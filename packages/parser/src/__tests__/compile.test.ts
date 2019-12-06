789a5b6bf5ff8c5e561851f86405f3f41ff63391
ricky <wqscut@gmail.com>
2019-12-06 13:55:32 +0800

    feat(parser): add typescript compile(supporting browser compiling)

---
+++ packages/parser/src/__tests__/compile.test.ts
@@ -0,0 +1,43 @@
+import { compile } from '../compile/index'
+
+const Demo = `
+function extra1(){
+  console.log('extra')
+}
+function extra2(name1:string,name2:string){
+  console.log(name1,name2)
+}
+class DemoBase {
+  private base:string;
+  constructor (name:string) {
+    this.base = name
+  }
+}
+class Demo extends DemoBase{
+  static active: boolean = false;
+  private secret: string|number;
+  private base:DemoBase;
+
+  constructor() {
+    super('demo')
+    this.secret = "cool";
+  }
+
+  public print() {
+    console.log(this.secret);
+  }
+
+  public greet(name:string) {
+    console.log(name)
+  }
+}
+
+export default Demo;`
+
+describe('Typescript Compile Test', () => {
+  test('runs without crashing', () => {
+    expect(() => {
+      compile(Demo)
+    }).not.toThrow()
+  })
+})
---
+++ packages/parser/src/compile/index.ts
@@ -0,0 +1,61 @@
+import * as ts from 'typescript'
+
+const defaultFileName = 'compile.ts'
+
+export function compile(content: string) {
+  // Create a compilerHost by hand because
+  // calling createCompilerHost will error
+  // out due to us being in a non-node
+  // environment.
+  //
+  // Ref: https://stackoverflow.com/questions/52969177/typescript-createprogram-throwing-ts-sys-is-undefined
+  // > If you don't pass a compiler host to createProgram, then the library tries to create a default host automatically based on the lower-level ts.sys object.  ts.sys in turn is only automatically generated in Node.js-like environments.
+  const compilerHost = {
+    getSourceFile: () =>
+      ts.createSourceFile(
+        defaultFileName,
+        content,
+        3, // ES5 // previously: compilerOptions.target,
+        false
+      ),
+    writeFile: () => {},
+    getDefaultLibFileName: () => 'lib.d.ts',
+    useCaseSensitiveFileNames: () => false,
+    getCanonicalFileName: fileName => fileName,
+    getCurrentDirectory: () => '',
+    getNewLine: () => '\n',
+    fileExists: () => true,
+    readFile: (fileName: string) => (fileName != null ? content : undefined)
+  }
+
+  const program = ts.createProgram(
+    [defaultFileName],
+    {
+      target: ts.ScriptTarget.ES5,
+      allowJs: true,
+      skipLibCheck: true,
+      esModuleInterop: true,
+      allowSyntheticDefaultImports: true,
+      strict: true,
+      forceConsistentCasingInFileNames: true,
+      module: ts.ModuleKind.ESNext,
+      moduleResolution: ts.ModuleResolutionKind.NodeJs,
+      resolveJsonModule: true,
+      isolatedModules: true,
+      noEmit: true,
+      jsx: ts.JsxEmit.Preserve
+    },
+    compilerHost
+  )
+  const checker = program.getTypeChecker()
+
+  function getSymbolByType<T>(declaration: T | ts.SourceFile) {
+    return checker.getSymbolAtLocation(declaration as ts.SourceFile)
+  }
+
+  const node = program.getSourceFile(defaultFileName)
+  const symbol = getSymbolByType(node)
+  return {
+    symbol
+  }
+}
