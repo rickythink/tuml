f6502472436551ae473665ef7440c59187b40fa6
ricky <wqscut@gmail.com>
2019-12-26 16:44:08 +0800

    build(parser): use parcel for development of parser

---
+++ packages/parser/public/index.ts
@@ -0,0 +1,39 @@
+import { Parser } from '../src/index'
+
+const Demo = `
+export function extra1(){
+  console.log('extra')
+}
+function extra2(name1:string,name2:string){
+  console.log(name1,name2)
+}
+class DemoBase {
+  private base:string;
+  constructor (name:string) {
+    this.base = name
+  }
+}
+class Demo extends DemoBase{
+  static active: boolean = false;
+  private secret: string|number;
+  private base:DemoBase;
+
+  constructor() {
+    super('demo')
+    this.secret = "cool";
+  }
+
+  public print() {
+    console.log(this.secret);
+  }
+
+  public greet(name:string) {
+    console.log(name)
+  }
+}
+
+export default Demo;`
+
+document.getElementById('root').innerHTML = `<pre><code>${Demo}</code></pre>`
+
+new Parser(Demo).parse()
