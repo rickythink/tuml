bc1b1e77a8949fc444ba4817f2065a1a220376b5
Ricky <wqscut@gmail.com>
2020-01-06 19:20:38 +0800

    build(uml): support svg as react component under typescript and storybook

---
+++ packages/uml/custom.d.ts
@@ -0,0 +1,7 @@
+declare interface SvgrComponent
+  extends React.StatelessComponent<React.SVGAttributes<SVGElement>> {}
+
+declare module '*.svg' {
+  const content: SvgrComponent
+  export default content
+}
